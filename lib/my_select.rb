e5eb5b0264c27d6775a1958d899740ba0de4b398
Kate Schlunz <schlunzk@gmail.com>
2018-03-02 16:37:27 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,15 +2,15 @@
 my_array = [1,2,3,4,5]
 
 def my_select(collection)
-
+  new_array = []
   i = 0
   while i < collection.length
-    if num % 2= 0
-   yield(collection[i])
+    if num % 2 = 0
+   new_array << yield(collection[i])
    i = i + 1
- end 
  end
- collection
+ end
+ new_array
 end
 
 
