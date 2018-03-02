e5a4d67540c870081a7a7ac32c2d6ccd9f21f182
Kate Schlunz <schlunzk@gmail.com>
2018-03-02 15:41:40 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,13 @@
 def my_select(collection)
- # your code here!
+  new_collection = []
+  i = 0
+  while i < collection.length
+   new_collection << yield(collection[i])
+   i = i + 1
+
+ end
+  new_collection
 end
+end
+
+[1,2,3,4,5].select { |num|  num.even?  }
